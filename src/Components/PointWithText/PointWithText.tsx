f45c22337d539bbf93558ee11c7bf2f3a0c290e5
AdamJochemczyk <adamjoche@gmail.com>
2023-02-10 15:22:19 +0100

    feat: for you desktop

--- src/Components/PointWithText/PointWithText.tsx
+++ src/Components/PointWithText/PointWithText.tsx
@@ -1,16 +1,18 @@
-import style from "./PointWithText.module.scss"
+import style from "./PointWithText.module.scss";
 
 export const PointWithText = ({
   pointNumber,
   primaryText,
   secondaryText,
+  wider=false,
 }: {
-  pointNumber:number;
-  primaryText:React.ReactNode;
-  secondaryText:React.ReactNode;
+  pointNumber: number;
+  primaryText: React.ReactNode;
+  secondaryText: React.ReactNode;
+  wider?: boolean;
 }) => {
   return (
-    <div className={style.container}>
+    <div className={`${style.container} ${wider ? style.restPoints : ""}`}>
       <div className={style.pointBox}>{pointNumber}</div>
       <div className={style.info}>
         <div className={style.primaryText}>{primaryText}</div>
