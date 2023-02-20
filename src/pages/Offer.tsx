d0b2c77d727df48b6e012ad6e888d0a3b27bbe48
Mateusz Grden <matgrden@gmail.com>
2023-02-20 21:30:53 +0100

    moved tables to mocks

--- src/pages/Offer.tsx
+++ src/pages/Offer.tsx
@@ -1,162 +1,18 @@
 import React from "react";
 import { Navigation } from "src/Components/Navigation/Navigation";
 import { PageTitle } from "src/Components/PageTitle/PageTitle";
+import { Button } from "src/Components/Buttons/Button";
 import styles from "./Offer.module.scss";
 import bowlOffer from "src/assets/Photos/bowl.png";
-import carrot from "src/assets/Photos/carrot.png";
-import grapefruit from "src/assets/Photos/grapefruit.png";
 import potOfVegetables from "src/assets/Photos/potOfVegetables.png";
 import personOnScale from "src/assets/Photos/personOnScale.png";
-
-import { Button } from "src/Components/Buttons/Button";
-
-const mobileAndOnlineDiets = [
-    {
-        src: carrot,
-        alt: "carrot",
-        text: "DIETETYK MOBILNY",
-    },
-    {
-        src: grapefruit,
-        alt: "grapefruit",
-        text: "DIETETYK ONLINE",
-    },
-];
-
-const offerData = [
-    {
-        title: "PIERWSZA KONSULTACJA DIETETYCZNA 120 ZŁ",
-        listText: [
-            "Omówienie wywiadu żywieniowego oraz wywiadu dotyczącego stylu życia",
-            "Omówienie wyników badań",
-            "Wstępna diagnoza i omówienie problemu",
-            "Wyznaczenie celu i kierunku dalszej współpracy",
-            "Wsparcie i motywację",
-        ],
-    },
-    {
-        title: "DRUGA KONSULTACJA DIETETYCZNA 150 ZŁ",
-        listText: [
-            "Omówienie przygotowanego planu żywieniowego wraz z listTextą zakupów",
-            "Edukacja żywieniowa",
-        ],
-    },
-    {
-        title: "KONSULTACJA KONTROLNA 80 ZŁ",
-        listText: [
-            "Analiza postępów dietoterapii",
-            "Przy wystąpieniu trudności, omówienie ich i zaproponowanie rozwiązania",
-            "Kontynuacja edukacji żywieniowej",
-        ],
-    },
-    {
-        title: "Konsultacja kontrolna z nowym planem żywieniowym",
-        price: "150 ZŁ",
-    },
-    {
-        title: "Dodatkowy indywidualny jadłospis 7-dniowy",
-        price: "150 ZŁ",
-    },
-    {
-        title: "Dodatkowy indywidualny jadłospis 14-dniowy",
-        price: "250 ZŁ",
-    },
-    {
-        title: "Jadłospis niestandardowy",
-        extraText: ["w przypadku wielu wykluczeń i alergii"],
-        price: "199 ZŁ",
-    },
-    {
-        title: "Edukacja żywieniowa",
-        extraText: ["pomoc w zmianie nawyków żywieniowych"],
-        price: "100 ZŁ",
-    },
-    {
-        title: "Dojazd do domu/w wyznaczone miejsce",
-        price: "DO 15 KM. GRATIS",
-    },
-    {
-        title: "Analiza składu ciała wraz z omówieniem wyników",
-        price: "50 ZŁ",
-    },
-];
-
-const bodyCompositionAnalyzer = [
-    "masa ciała",
-    "masa tkanki tłuszczowej",
-    "poziom tłuszczu trzewnego",
-    "masa mięśni szkieletowych",
-    "całkowita zawartość wody w organizmie",
-    "podstawowa przemiana materii",
-    "BMI",
-];
-
-const preparationForMedicalExamination = [
-    "Zachowaj 3 godzinną przerwę między ostatnim posiłkiem a badaniem",
-    "Dobę przed badaniem nie pij alkoholu",
-    "12 godzin przed badaniem nie wykonuj intensywnych ćwiczeń",
-    "Ok. 30 minut przed badaniem skorzystaj z toalety",
-    "Przed badaniem zdejmij wszystkie metalowe przedmioty",
-    "Dłonie i stopy nie powinny być natłuszczone kremem lub balsamem",
-];
-
-const packages = [
-    {
-        title: "NA DOBRY POCZĄTEK",
-        additionalTitle: ["1 miesiąc wsparcia", "299 zł / miesiąc"],
-        paragraphs: [
-            "Pakiet idealny dla osób rozpoczynających przygodę z dietetykiem.",
-            "Obejmuje pierwszą i konsultacja z omówieniem planu żywieniowego oraz konsultację kontrolną.",
-        ],
-        list: [
-            "Omówienie wywiadu żywieniowego oraz wywiadu dotyczącego stylu życia",
-            "Omówienie wyników badań",
-            "Wstępną diagnozę i omówienie problemu",
-            "Wyznaczenie celu i kierunku dalszej współpracy",
-            "Indywidualny, elastyczny jadłospis 7 dniowy",
-            "Listę zakupów",
-            "Regularny kontakt i wsparcie dietetyka",
-        ],
-    },
-    {
-        title: "ZMIANA NA DOBRE",
-        additionalTitle: ["3 miesiące wsparcia", "216 zł / miesiąc"],
-        extraTitle: "897 zł 648 zł łącznie",
-        paragraphs: [
-            "Pakiet idealny dla osób chcących realizować swoje większe cele dietetyczne.",
-            "Obejmuje pierwszą konsultację, 3 konsultacje z omówieniem nowego planu żywieniowego oraz 3 konsultacje kontrolne.",
-        ],
-        list: [
-            "Omówienie wywiadu żywieniowego oraz wywiadu dotyczącego stylu życia",
-            "Omówienie wyników badań",
-            "Wstępną diagnozę i omówienie problemu",
-            "Wyznaczenie celu i kierunku dalszej współpracy",
-            "Indywidualny, elastyczny jadłospis 7 dniowy, na każdy nowy miesiąc",
-            "Listę zakupów",
-            "Utrwalenie nowych nawyków żywieniowych w formie zadań kontrolnych",
-            "Regularny kontakt i wsparcie dietetyka",
-        ],
-    },
-    {
-        title: "TOTALNA METAMORFOZA",
-        additionalTitle: ["6 miesiące wsparcia", "199 zł / miesiąc"],
-        extraTitle: "1794 zł 1194 zł łącznie",
-        paragraphs: [
-            "Pakiet idealny dla osób, które chcą osiągać więcej.",
-            "Obejmuje pierwszą konsultację, 6 konsultacji z omówieniem nowego planu żywieniowego oraz 6 konsultacji kontrolnych.",
-        ],
-        list: [
-            "Omówienie wywiadu żywieniowego oraz wywiadu dotyczącego stylu życia",
-            "Omówienie wyników badań",
-            "Wstępną diagnozę i omówienie problemu",
-            "Wyznaczenie celu i kierunku dalszej współpracy",
-            "Indywidualny, elastyczny jadłospis 7 dniowy, na każdy nowy miesiąc",
-            "Listę zakupów",
-            "Utrwalenie nowych nawyków żywieniowych w formie zadań kontrolnych",
-            "Regularny kontakt i wsparcie dietetyka",
-        ],
-    },
-];
+import {
+    MOBILE_AND_ONLINE_DIETS,
+    OFFER_DATA,
+    BODY_COMPOSITION_ANALYZER,
+    PREPARATION_FOR_MEDICAL_EXAMINATION,
+    PACKAGES,
+} from "src/Utilities/mocks";
 
 export const Offer = () => {
     return (
@@ -190,7 +46,7 @@ export const Offer = () => {
                         </div>
                     </div>
                     <div className={styles.mobileAndOnlineDiets}>
-                        {mobileAndOnlineDiets.map(({ src, alt, text }) => {
+                        {MOBILE_AND_ONLINE_DIETS.map(({ src, alt, text }) => {
                             return (
                                 <div
                                     key={text + src}
@@ -231,7 +87,7 @@ export const Offer = () => {
                         </div>
                     </div>
                     <div className={styles.boxOfferContainer}>
-                        {offerData.map(offer => {
+                        {OFFER_DATA.map(offer => {
                             if (offer.listText) {
                                 return (
                                     <div key={offer.title} className={styles.boxOffer}>
@@ -309,7 +165,7 @@ export const Offer = () => {
                                 bezpiecznym pomiarze parametrów ciała m.in. takich jak:
                             </p>
                             <ul className={styles.boxOffer__list}>
-                                {bodyCompositionAnalyzer.map(text => (
+                                {BODY_COMPOSITION_ANALYZER.map(text => (
                                     <li key={text} className={styles.boxOffer__list__item}>
                                         {text}
                                     </li>
@@ -338,7 +194,7 @@ export const Offer = () => {
                     <div className={styles.boxOffer}>
                         <p className={styles.boxOffer__title}>PRZYGOTOWANIE DO BADANIA</p>
                         <ul className={styles.boxOffer__list}>
-                            {preparationForMedicalExamination.map(text => (
+                            {PREPARATION_FOR_MEDICAL_EXAMINATION.map(text => (
                                 <li key={text} className={styles.boxOffer__list__spacedItem}>
                                     {text}
                                 </li>
@@ -360,7 +216,7 @@ export const Offer = () => {
                 </section>
                 <section>
                     <h2 className={styles.header}>PAKIETY</h2>
-                    {packages.map(itemPackage => (
+                    {PACKAGES.map(itemPackage => (
                         <div key={itemPackage.title} className={styles.boxOffer}>
                             <div className={styles.boxOffer__withButton}>
                                 <div className={styles.textContainer__halfScreen}>
