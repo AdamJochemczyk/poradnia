4bb1700eb7daab3f5ce863b1f2eaf7ca60e15cc9
AdamJochemczyk <adamjoche@gmail.com>
2023-03-04 16:08:33 +0100

    main page fixes

--- src/Components/Fundamentals/Fundamentals.tsx
+++ src/Components/Fundamentals/Fundamentals.tsx
@@ -1,24 +1,62 @@
-import style from "./Fundamentals.module.scss"
-import { FUNDAMENTALS } from "../../Utilities/mocks"
+import style from "./Fundamentals.module.scss";
+import fork from "../../assets/Graphics/fork.svg";
+import cart from "../../assets/Graphics/cart.svg";
+import heart from "../../assets/Graphics/heart.svg";
+import stetoscope from "../../assets/Graphics/stetoscope.svg";
+import apple from "../../assets/Graphics/time.svg";
 
 function Fundamentals() {
   return (
     <div className={style.fundamentals}>
-        <h2 className={style.fundamentals__header}>FUNDAMENTY SKUTECZNEJ DIETY</h2>
-        <div className={style.content}>
-            <div className={style.content__desktop}>
-                {FUNDAMENTALS.map((element, index) => {
-                    return (
-                        <div className={style.content__element} key={index}>
-                            <img src={element.image} alt="fundamenty" className={style.image} />
-                            <p className={style.text}><span className={style.bold}>{element.title}</span> {element.description}</p>
-                        </div>
-                    )
-                })}
-            </div>
+      <h2 className={style.fundamentals__header}>
+        FUNDAMENTY SKUTECZNEJ DIETY
+      </h2>
+      <div className={style.content}>
+        <div className={style.content__desktop}>
+          <div className={style.content__element}>
+            <img src={cart} alt="cart" className={style.image} />
+            <p className={style.text}>
+              <span className={style.bold}>
+                Ogólnodostępne i niedrogie produkty,
+              </span>{" "}
+              które znajdziesz w lokalnych sklepach.
+            </p>
+          </div>
+          <div className={style.content__element}>
+            <img src={fork} alt="fork" className={style.image} />
+            <p className={style.text}>
+              Ty mówisz <span className={style.bold}>co lubisz,</span><br /> a ja
+              mówię Ci <span className={style.bold}>jak i kiedy to jeść.</span>
+            </p>
+          </div>
+          <div className={style.content__element}>
+            <img src={apple} alt="apple" className={style.image} />
+            <p className={style.text}>
+              <span className={style.bold}>Czas </span>
+              przygotowania posiłków dostosowany do Ciebie.
+            </p>
+          </div>
+          <div className={style.content__element}>
+            <img src={stetoscope} alt="stetoscope" className={style.image} />
+            <p className={style.text}>
+              <span className={style.bold}>
+                Indywidualnie dopasowana dieta,
+              </span>{" "}
+              na podstawie Twoich wyników badań i szczegółowego wywiadu.
+            </p>
+          </div>
+          <div className={style.content__element}>
+            <img src={heart} alt="heart" className={style.image} />
+            <p className={style.text}>
+              <span className={style.bold}> Wsparcie dietetyka,</span> nie tylko
+              na wizytach kontrolnych. Jeśli tego potrzebujesz, możemy być w
+              stałym kontakcie!
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Fundamentals
+export default Fundamentals;
--- src/Utilities/mocks.ts
+++ src/Utilities/mocks.ts
@@ -1,41 +1,7 @@
-import fork from "../assets/Graphics/fork.svg";
-import cart from "../assets/Graphics/cart.png";
-import heart from "../assets/Graphics/heart.png";
-import stetoscope from "../assets/Graphics/stetoscope.png";
-import apple from "../assets/Graphics/Group.png";
 import carrot from "src/assets/Photos/carrot.png";
 import carrotLarge from "src/assets/Photos/carrotLarge.png";
 import grapefruit from "src/assets/Photos/grapefruit.png";
 import grapefruitLarge from "src/assets/Photos/grapefruitLarge.png";
-
-export const FUNDAMENTALS = [
-
-    {
-        title: "Ogólnodostępne i niedrogie produkty,",
-        description: "które znajdziesz w lokalnych sklepach.",
-        image: cart
-    },
-    {
-        title: "Ty mówisz co lubisz,",
-        description: " a ja mówię Ci jak i kiedy to jeść.",
-        image: fork
-    },
-    {
-        title: "Czas",
-        description: " przygotowania posiłków dostosowany do Ciebie.",
-        image: apple
-    },
-    {
-        title: "Indywidualnie dopasowana dieta,",
-        description: " na podstawie Twoich wyników badań szczegółowego wywiadu.",
-        image: stetoscope
-    },
-    {
-        title: "Wsparcie dietetyka,",
-        description: `nie tylko na wizytach kontrolnych. Jeśli tego potrzebujesz, możemy być w stałym kontakcie! `,
-        image: heart
-    },
-];
 
 export const MOBILE_AND_ONLINE_DIETS = [
     {
